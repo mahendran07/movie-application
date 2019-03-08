3419d1fb2e8bf359ac436fd57c358a50b34bfc6c
mahe2111 <mahe2111@CSLH1707.global.chainsys.com>
2019-03-08 06:05:15 +0530

    changes redirect path in register page

--- src/com/chainsys/movieapplication/controller/RegisterServlet.java
+++ src/com/chainsys/movieapplication/controller/RegisterServlet.java
@@ -29,13 +29,6 @@ public class RegisterServlet extends HttpServlet {
     }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	}
-
-	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -53,7 +46,7 @@ public class RegisterServlet extends HttpServlet {
 		try {
 			registerdao.addUser(register);
 			RequestDispatcher rd = request
-					.getRequestDispatcher("Login.html");
+					.getRequestDispatcher("Login.jsp");
 			rd.forward(request, response);
 		} catch (Exception e) {
 			e.printStackTrace();
